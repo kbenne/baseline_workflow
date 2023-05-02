bb1993e5ce0083fd9fa9ad895d663f40bc71020f
Kyle Benne <Kyle.Benne@nrel.gov>
2023-05-02 10:24:30 -0500

    Run baseline workflow

--- example_osw/measures/run_baseline/measure.rb
+++ example_osw/measures/run_baseline/measure.rb
@@ -27,17 +27,28 @@ class RunBaseline < OpenStudio::Measure::ModelMeasure
     workflow = runner.workflow
 
     # Directory for baseline run
-    baseline_run_dir = File.join(workflow.absoluteRunDir.to_s, 'baseline')
-    FileUtils.mkdir_p(baseline_run_dir)
+    baseline_run_dir = workflow.absoluteRunDir / OpenStudio::Path.new('baseline')
+    FileUtils.mkdir_p(baseline_run_dir.to_s)
 
     # Create baseline osm model
-    baseline_osm_name = File.join(baseline_run_dir, 'baseline.osm')
-    model.clone.save(baseline_osm_name, true)
+    baseline_osm_path = baseline_run_dir / OpenStudio::Path.new('baseline.osm')
+    model.clone.save(baseline_osm_path, true)
 
     # Copy weather file
-    weather_file = workflow.findFile(workflow.weatherFile.get).get.to_s
+    weather_file_path = workflow.findFile(workflow.weatherFile.get).get
+    FileUtils.cp(weather_file_path.to_s, baseline_run_dir.to_s)
 
-    FileUtils.cp(weather_file, baseline_run_dir)
+    # Create a workflow to run the baseline
+    baseline_workflow = OpenStudio::WorkflowJSON.new
+    baseline_workflow.setSeedFile(baseline_osm_path)
+    baseline_workflow.setWeatherFile(weather_file_path)
+    baseline_workflow_path = baseline_run_dir / OpenStudio::Path.new('baseline.osw')
+    baseline_workflow.saveAs(baseline_workflow_path)
+
+    # Run the baseline workflow
+    cli_path = OpenStudio.getOpenStudioCLI
+    cmd = "\"#{cli_path}\" run -w \"#{baseline_workflow_path.to_s}\""
+    system(cmd)
 
     return true
   end
